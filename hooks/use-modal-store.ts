e102894f439af865b33d92d352f3bdd851acdf7f
vinitborad <vinitborad12@gmail.com>
2023-08-31 15:49:42 +0530

    Implemented Create Server Button Functionality

---
+++ hooks/use-modal-store.ts
@@ -0,0 +1,29 @@
+import { Server, Channel, ChannelType } from "@prisma/client"
+import { create } from "zustand"
+//WHY : what is zustand
+
+export type ModalType = "createServer";
+
+interface ModalData {
+    server?: Server;
+    channel?: Channel;
+    channelType?: ChannelType;
+    apiUrl?: string;
+    query?: Record<string, any>;
+}
+
+interface ModalStore {
+    type: ModalType | null;
+    data: ModalData;
+    isOpen: boolean;
+    onOpen: (type: ModalType, data?: ModalData) => void;
+    onClose: () => void;
+}
+
+export const useModal = create<ModalStore>((set) => ({
+    type: null,
+    data: {},
+    isOpen: false,
+    onOpen: (type, data = {}) => set({ isOpen: true, type, data }),
+    onClose: () => set({ type: null, isOpen: false })
+}));
